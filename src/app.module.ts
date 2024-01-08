8039246ccac095fa1775fe6e71fbac839a8c62b8
Adrian333Dev <adrian333dev@gmail.com>
2024-01-09 02:16:13 +0400

    save changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './resources/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017', {
+      dbName: 'steam_line_db',
+    }),
+    ,
+    UserModule,
+  ],
 })
 export class AppModule {}
